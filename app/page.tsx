a06591d24a7e8b03ac959a9ef23707bafe76ceb3
Bernesh-stack <bernesh.in@gmail.com>
2025-04-16 13:27:28 +0530

    data fetch through serever example

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,26 @@ import Ivaru from "./Components/Doop";
 
 export default function Home() {
   console.log("Hello world");
+
+
+
+// How to fetch from the server is noted with example 
+  // export async function generateMetadata() {
+  //   const data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  //   const json = await data.json();
+  //   return {
+  //     title: json.title,
+  //     description: json.body,
+  //   };
+  // }
+
+
+
+
   return (
     <>
-   <h2 className="text-cyan-800">Hi hello </h2>
-   <Ivaru/>
+
+
    </>
   );
 }
